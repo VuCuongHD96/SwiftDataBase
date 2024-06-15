94030892676d8cfad9e43578a0413a1d42a302b8
VuCuongHD96 <xuancuonghd1102@gmail.com>
2024-06-15 08:49:54 +0700

    Initial Commit

---
+++ SwiftDataBase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataBase
+//
+//  Created by Work on 15/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataBase/SwiftDataBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataBaseApp.swift
+//  SwiftDataBase
+//
+//  Created by Work on 15/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
