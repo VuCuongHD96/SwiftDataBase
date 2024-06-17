4bc1cc9a81508fbd7211bb5c9543ef65c37a12ca
VuCuongHD96 <xuancuonghd1102@gmail.com>
2024-06-17 21:41:57 +0700

    implement swift data

--- SwiftDataBase/ContentView.swift
+++ SwiftDataBase/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let repository: any PersonRepositoryType = PersonRepository()
+    @State private var person: [Person] = []
+    
     var body: some View {
         VStack {
+            Button("Add Person") {
+                repository.addPerson()
+                person = repository.getPersonList()
+                repository.swiftDataManager.save()
+            }
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            List {
+                ForEach(person) {
+                    Text($0.name)
+                }
+                .onDelete(perform: { indexSet in
+                    indexSet.forEach {
+                        let person = person[$0]
+                        repository.delete(object: person)
+                        repository.swiftDataManager.save()
+                    }
+                })
+            }
         }
         .padding()
+        .onAppear {
+            person = repository.getPersonList()
+            repository.swiftDataManager.save()
+        }
     }
 }
 
